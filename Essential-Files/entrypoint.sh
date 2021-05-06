456b6c7d9c7eb3bce27a0bccfe9c4cf0f916dbf4
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-06 14:03:53 +0530

    Update entrypoint.sh

--- Essential-Files/entrypoint.sh
+++ Essential-Files/entrypoint.sh
@@ -2,5 +2,5 @@
 mv /Essential-Files/Aria2Rclone.htpy  /Essential-Files/Aria2Rclone.ipynb
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 jupyter serverextension enable voila --sys-prefix
-curl 'https://github.com/developeranaz/Aria2-Rclone-Remote-Uploader-HEROKU/d/'
+curl 'https://raw.githubusercontent.com/developeranaz/Aria2-Rclone-Remote-Uploader-HEROKU/main/Essential-Files/d/donate.developeranaz.txt'
 voila --port=$PORT --no-browser  /Essential-Files/Aria2Rclone.ipynb --theme=dark
