5f50860791dabd95ece558f0446364ff559af2c4
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-08-17 23:12:26 +0530

    Update entrypoint.sh

--- Essential-Files/entrypoint.sh
+++ Essential-Files/entrypoint.sh
@@ -4,8 +4,7 @@ mkdir /.config
 mkdir /.config/rclone/
 wget -nc $CONFIG_FILE_IN_URL -O '/.config/rclone/rclone.conf'
 rclone version
-rclone version
-rclone listremotes |grep ']' |sed 's/\[/    "acykic774hl/g' | sed 's|]|acykic774hl, \\enacykic774hl",|g' |sed 's/enacykic774hl/n/g' |sed "s/acykic774hl/'/g" >remotes05.txt
+rclone listremotes |sed 's/://g' |sed "s/^/'/g" |sed "s/$/',en-g0e-s-wr0ng-unknown-hash-182/g" |sed 's/en-g0e-s-wr0ng-unknown-hash-182/en-g0e-s-wr0ng-unknown-hash-182en-g0e-s-wr0ng-unknown-hash-684/g'|sed 's/en-g0e-s-wr0ng-unknown-hash-182/\\/g' |sed 's/en-g0e-s-wr0ng-unknown-hash-684/n/g' |sed 's/^/    "/g' |sed 's/$/",/g' >remotes05.txt
 cat remotes05.txt >> 1.htpy
 cat 2 >>1.htpy
 mv /1.htpy  /Aria2Rclone.ipynb
