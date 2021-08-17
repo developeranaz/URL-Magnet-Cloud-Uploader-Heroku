5f35c25335830b254ac548b5f62b1d30b47ae03c
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-08-17 20:17:22 +0530

    Update entrypoint.sh

--- Essential-Files/entrypoint.sh
+++ Essential-Files/entrypoint.sh
@@ -5,14 +5,7 @@ mkdir /.config/rclone/
 wget -nc $CONFIG_FILE_IN_URL -O '/.config/rclone/rclone.conf'
 rclone version
 rclone version
-rclone listremotes > remotes.txt
-sed -i 's/.$//' remotes.txt
-sed "s/[^ ][^ ]*/'&'/g" remotes.txt >remotes01.txt
-sed '$!s/$/,/' remotes01.txt >remotes02.txt
-sed -e 's/$/ \\/' remotes02.txt >remotes03.txt
-sed -i s/$/n/ remotes03.txt
-sed 's/\(.*\)/"\1"/g' remotes03.txt >remotes04.txt
-sed 's/$/,/' remotes04.txt >remotes05.txt
+rclone listremotes |grep ']' |sed 's/\[/    "acykic774hl/g' | sed 's|]|acykic774hl, \\enacykic774hl",|g' |sed 's/enacykic774hl/n/g' |sed "s/acykic774hl/'/g" >remotes05.txt
 cat remotes05.txt >> 1.htpy
 cat 2 >>1.htpy
 mv /1.htpy  /Aria2Rclone.ipynb
