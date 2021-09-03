ed4e41a3f390edc2000120d2eabfc2d6304f92c0
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-03 23:05:25 +0530

    Update config.sh

--- Essential-Files/config.sh
+++ Essential-Files/config.sh
@@ -1,5 +1,3 @@
-
 mv /Essential-Files/Aria2Mega.nz /Aria2Mega.ipynb
 cp /Essential-Files/requirements.txt /requirements.txt
 cp /Essential-Files/Aria2Mega.jpg /Aria2Mega.jpg
-cp /Essential-Files/jconf.py /conf/jupyter.py
