ce91f52bc98be4ecabf026fd6e62548a09af6ff9
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-09-03 23:04:36 +0530

    Update config.sh

--- Essential-Files/config.sh
+++ Essential-Files/config.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-cp /Essential-Files/index.html /usr/index.html
+ /usr/index.html
 mv /Essential-Files/Aria2Mega.nz /Aria2Mega.ipynb
 cp /Essential-Files/requirements.txt /requirements.txt
 cp /Essential-Files/Aria2Mega.jpg /Aria2Mega.jpg
