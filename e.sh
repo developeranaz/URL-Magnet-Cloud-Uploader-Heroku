fd973aacbc77ad2f4b6128cf83949203d694340f
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-11-03 10:51:38 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo $PORT >PORT
-curl 'https://raw.githubusercontent.com/developeranaz/URL-Magnet-Cloud-Uploader-Heroku/Changing-webapp-to-Flask-python/app.py' | sed "s|4996|$(cat PORT)|g" > /web/app.py 
+curl 'https://raw.githubusercontent.com/developeranaz/URL-Magnet-Cloud-Uploader-Heroku/Changing-webapp-to-Flask-python/app.py' | sed "s|THERANDOMPORT|$(cat PORT)|g" > /web/app.py 
 curl "$CONFIG_FILE_IN_URL" > $(rclone config file |grep rclone.conf)
 python3 /web/app.py
