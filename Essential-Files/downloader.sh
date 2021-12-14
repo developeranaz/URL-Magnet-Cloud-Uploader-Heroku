1b16641f61127a90190efd8840223dce6e4d66fd
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2021-12-14 20:28:52 +0530

    Update downloader.sh

--- Essential-Files/downloader.sh
+++ Essential-Files/downloader.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-mkdir /FILES/DOWNLOADS/$$
-cd /FILES/DOWNLOADS/$$
+mkdir ./FILES/$$
+cd ./FILES/$$
+mkdir ./DOWNLOADS
+threads=$(cat config.txt |head -1)
+cat urlraw.txt |sed 's| |/n|g' |sed "s|@;| |g"|sed "s|^|aria2c -x$threads '|g" |sed "s|$|'|g"
 
